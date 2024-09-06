f237f19502671dfc9a5e86382141b59fd0a68dd4
vikas pokhriyal <pokhriyalvikas940@gmail.com>
2024-09-06 11:36:02 +0530

    set

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import styles from './contact.module.css'
 
 const Contact = () => {
--- src/app/docs/[...slug]/page.jsx
+++ src/app/docs/[...slug]/page.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 
-const page = ({ params }) => {
-  if (params.slug.length==2) {
-    
-  }
+const page = () => {
   return (
     <div className='container'>
       <p>This is Docs </p>
