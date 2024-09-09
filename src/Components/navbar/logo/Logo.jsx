00b72a6fb5ce11ba587f5bc2d6a8b6f1f8110f30
vikas pokhriyal <pokhriyalvikas940@gmail.com>
2024-09-09 13:50:25 +0530

    hmm

--- src/Components/navbar/logo/Logo.jsx
+++ src/Components/navbar/logo/Logo.jsx
@@ -1,12 +1,13 @@
 
 import Link from 'next/link'
 import styles from './logo.module.css'
+import Image from 'next/image'
 
 const Logo = () => {
   return (
     <div className={styles.logo}>
           <Link href='/'>
-             Logo
+            <Image width={40} height={40} src='/logoicon2.png' alt='logo'/>
           </Link>
     </div>
   )
