1f63cbb918393b1c825fd04fda5d2f8b2a0c041f
vikas pokhriyal <pokhriyalvikas940@gmail.com>
2024-09-10 10:53:09 +0530

    button add in docs

--- src/app/docs/[...slug]/page.jsx
+++ src/app/docs/[...slug]/page.jsx
@@ -1,9 +1,18 @@
+'use client';
 
-const page = ({params}) => {
-  
+import { useRouter } from 'next/navigation'
+import styles from './docs.module.css'
+
+const page = ({ params }) => {
+  const router = useRouter();
+
+  const handleRouter = () => {
+    router.back();
+  }
   return (
     <div className='container'>
       <p>This is Docs {params.slug}</p>
+      <button className={styles.btn} onClick={handleRouter}>previous</button>
     </div>
   )
 }
--- src/app/docs/page.jsx
+++ src/app/docs/page.jsx
@@ -1,9 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
+
   return (
     <div className='container'>
       <p>Docs</p>
+      <ul>
+        <li>
+          <Link href={'/docs/1'}>Docs-1</Link>
+        </li>
+        <li>
+          <Link href={'/docs/2'}>Docs-2</Link>
+        </li>
+        <li>
+          <Link href={'/docs/3'}>Docs-3</Link>
+        </li>
+        <li>
+          <Link href={'/docs/next.js'}>Docs-4</Link>
+        </li>
+      </ul>
     </div>
   )
 }
